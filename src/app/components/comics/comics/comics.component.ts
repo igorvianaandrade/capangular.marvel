c18a5466346c017c877f564379842b7f7f4d54a2
igorvianaandrade <vianaigo@gmail.com>
2020-12-17 17:29:00 -0300

    teste

--- src/app/components/comics/comics/comics.component.ts
+++ src/app/components/comics/comics/comics.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ComicList } from 'src/app/models/comics/comic-list.model';
 import { ComicsService } from 'src/app/services/comics/comics.service';
 
+
 @Component({
   selector: 'app-comics',
   templateUrl: './comics.component.html',
